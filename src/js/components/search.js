8dafeb037ef0036f4e99b1323da8416db6f82ebd
Rui-Santos <rui.s@gmx.pt>
2018-03-17 23:57:09 +0000

    Remove api key check and update query params.

--- src/js/components/search.js
+++ src/js/components/search.js
@@ -1,10 +1,9 @@
-var APIKeyCheck = require('./apiKeyCheck');
-var Geocoder = require('leaflet-geocoder-mapzen/src/core');
+var Geocoder = require('tpp-leaflet-geocoder/src/core');
 var corslite = require('@mapbox/corslite');
 
 Geocoder.prototype.getSearchResult = function (input, callback) {
   var param = {
-    text: input
+    q: input
   };
   var params = this.getParams(param);
   corslite(this.options.url + '/search?' + this.serialize(params), callback, true);
@@ -12,27 +11,25 @@ Geocoder.prototype.getSearchResult = function (input, callback) {
 
 Geocoder.prototype.getAutocompleteResult = function (input, callback) {
   var param = {
-    text: input
+    q: input
   };
   var params = this.getParams(param);
-  corslite(this.options.url + '/autocomplete?' + this.serialize(params), callback, true);
+  corslite(this.options.url + '/search?' + this.serialize(params), callback, true);
 };
 
 module.exports = Geocoder;
 
-module.exports.geocoder = function (key, options) {
-  var params = APIKeyCheck.getKeyAndOptions(key, options);
+module.exports.geocoder = function (options) {
+  var params = options;
 
   // redirect search url to geocode.earth unless service url is speciied
   if (!params.options || !params.options.url) {
-    params.options = L.extend({ url: 'https://api.geocode.earth/v1' }, params.options);
+    params.options = L.extend({ url: 'https://nominatim.openstreetmap.org' }, params.options);
   }
 
   // If there is no attribution user passes,
   // Geocoder will skip the attribution since mapzen.js's map compoent is handling it already.
   if (params.options && !params.options.attribution) params.options.attribution = '';
   
-  if (!params.key) APIKeyCheck.throwApiKeyWarning('Search (geocode.earth)');
-  
-  return new Geocoder(params.key, params.options);
+  return new Geocoder(params.options);
 };
